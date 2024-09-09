2105ca2a0fe2a3ac9d194627561acab06e5bcd60
Gabriel Antonelli <150174957+G-Antonelli@users.noreply.github.com>
2024-09-09 13:35:25 -0600

    Updated root page and week-2 components with new structure and links

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Gabriel Antonelli</h1>
+      <Link href="https://github.com/G-Antonelli/cprg306-assignments.git">
+        https://github.com/G-Antonelli/cprg306-assignments.git
+      </Link>
+    </main>
+  );
+}
